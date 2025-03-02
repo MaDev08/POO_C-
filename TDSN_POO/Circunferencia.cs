aa452e3f6f4ba0c6ae646dec934e08485398c075
MaDev08 <mateusudo32@gmail.com>
2025-03-02 15:31:26 -0300

    Add files via upload

---
+++ TDSN_POO/Circunferencia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TDSN2024;
+namespace TDSNformas
+{
+    class Circunferencia : FormaGeometrica
+    {
+        private double raio;
+
+        public double Raio
+        {
+            get { return raio; }
+            set { raio = value; }
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return raio * 2 * 3.14;
+        }
+        public override double CalcularArea()
+        {
+            return 3.14 * Math.Pow(raio,2);
+        }
+        public override string ToString()
+        {
+            return $"Circunferência({raio})";
+        }
+    }
+}
